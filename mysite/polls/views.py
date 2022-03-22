b1b2d848e1a77a94878585171b2f1797e8c00b32
John Merigliano <jmerigliano@gmail.com>
2022-03-21 17:59:08 -0700

    added background and made the styles more appealing.

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,9 +1,7 @@
 import re
 
-from django.http import HttpResponse, HttpResponseRedirect
-from django.http import Http404
+from django.http import Http404, HttpResponse, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
-from django.shortcuts import render
 from django.template import loader
 from django.urls import reverse
 from django.utils import timezone
